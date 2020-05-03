d5bd0aa9123824f2e73f8fb975c1b2627f1c5cce
7UsmanS <59475843+7UsmanS@users.noreply.github.com>
2020-05-02 22:25:30 -0700

    Add existing file

--- src/BlockGridTest3.java
+++ src/BlockGridTest3.java
@@ -21,9 +21,11 @@ class BlockGridTest {
 		gridTest = new BlockGrid(10,20);
 		int actualWidth = gridTest.getWidth();
 		int actualHeight = gridTest.getHeight();
-		
 		assertEquals(10, actualWidth);
 		assertEquals(20, actualHeight);
+		
+		//Color actualColor = gridTest.getSquare(1,2);
+		//System.out.println("color from testBlockGrid " + actualColor.toString());
 	}
 
 	//Testing the DrawSquare(int,int)
@@ -33,8 +35,9 @@ class BlockGridTest {
 		gridTest = new BlockGrid(3,4);
 		gridTest.drawSquare(1,2);
 		Color actualColor = gridTest.getSquare(1,2);
+		// expectedColor = Color.WHITE; 
 		Boolean matchValue = actualColor.equals(Color.WHITE);
-		
+		//Boolean matchValue = gridTest.getSquare(1,2).equals(Color.WHITE);
 		assertEquals(true, matchValue);
 	}
 	
@@ -45,8 +48,8 @@ class BlockGridTest {
 		gridTest = new BlockGrid(5,4);
 		gridTest.drawSquare(pointTest);
 		Color actualColor = gridTest.getSquare(pointTest);
+		// expectedColor = Color.WHITE; 
 		Boolean matchValue = actualColor.equals(Color.WHITE);
-		
 		assertEquals(true, matchValue);
 	}
 
@@ -56,8 +59,8 @@ class BlockGridTest {
 		gridTest = new BlockGrid(1,2);
 		gridTest.setColor(Color.BLUE);
 		Color actualColor = gridTest.getColor();
+		//System.out.println("color from testSetColor" + actualColor.toString());
 		Boolean matchValue = actualColor.equals(Color.BLUE);
-		
 		assertEquals(true, matchValue);
 	}
 
@@ -67,7 +70,6 @@ class BlockGridTest {
 		gridTest = new BlockGrid(7,1);
 		int actualWidth = gridTest.getGraphicsWidth();
 		int expectedWidth = 7 * 25;
-		
 		assertEquals(expectedWidth, actualWidth);
 	}
 
@@ -77,7 +79,6 @@ class BlockGridTest {
 		gridTest = new BlockGrid(1,7);
 		int actualHeight = gridTest.getGraphicsHeight();
 		int expectedHeight = 7 * 25;
-		
 		assertEquals(expectedHeight, actualHeight);
 	}
 
@@ -89,17 +90,24 @@ class BlockGridTest {
 		gridTest.setColor(Color.GREEN);
 		gridTest.drawSquare(1,2);
 		Color prevColor = gridTest.getSquare(1,2);
+		//System.out.println("color from testClear before clear " + prevColor.toString());
 		Boolean matchValue = prevColor.equals(Color.GREEN);
-		
 		assertEquals(true, matchValue);
-		
 		gridTest.clear();
 		int actualWidth = gridTest.getWidth();
 		int actualHeight = gridTest.getHeight();
-		
 		assertEquals(3, actualWidth);
 		assertEquals(5, actualHeight);
 		assertNotNull(gridTest);
+		
+		/*Color newColor = gridTest.getSquare(1,1);
+		BlockGrid newGrid = new BlockGrid(3,5);
+		//System.out.println("color from testClear newGrid " + newGrid.toString());
+		//System.out.println("color from testClear after clear " + gridTest.toString());
+		Boolean value = gridTest.equals(newGrid.getSquare(1,1));
+		//assertNotNull(gridTest.getSquare(3,5));
+		//assertNull(newColor);
+		assertEquals(false, value);*/
 	}
 
 	//Testing Draw()
@@ -108,12 +116,14 @@ class BlockGridTest {
 		BlockGrid gridnullTest = new BlockGrid(0,0);
 		gridnullTest.draw(null);
 		Color old = gridnullTest.getColor();
+		//System.out.println("color from testDraw for null " + old.toString());
 		Boolean matchValue = old.equals(Color.WHITE);
-		
 		assertEquals(matchValue, true);
 
 		BlockGrid gridTest = new BlockGrid(5,7);
+
 		TetrisBlock.RIGHT_L.setLocation(3,5).draw(gridTest);
+
 		int iterations = 0;
 		
 		for(int i = 0; i < gridTest.getWidth(); i++) {
@@ -123,12 +133,15 @@ class BlockGridTest {
 		} 
 		
 		assertEquals(35, iterations); 
-		
+        //setForeground(Color.RED);  
 		TetrisBlock.RIGHT_L.setLocation(5,3).draw(gridTest);
 		TetrisBlock.SQUARE.setLocation(2,3).draw(gridTest);
 		TetrisBlock.LEFT_L.setLocation(5,7).draw(gridTest);
 		TetrisBlock.TEE.setLocation(3,2).draw(gridTest);
 		TetrisBlock.LONG .setLocation(1,2).draw(gridTest);
+
+ 
+		//TetrisBlock b = TetrisBlock.SQUARE.setLocation(5,2);
 	}
 
 	//Testing ToString()
@@ -136,13 +149,11 @@ class BlockGridTest {
 	void testToString() {
 		BlockGrid gridNullTest = new BlockGrid(3,2);
 		String stringValue = gridNullTest.toString();
-		
 		assertEquals("---\n" + "---", stringValue);
 		
 		BlockGrid gridTest = new BlockGrid(2,4);
 		gridTest.drawSquare(1,2);
 		String stringValueNew = gridTest.toString();
-		
 		assertEquals("--\n" + "--\n" + "-#\n" + "--", stringValueNew);
 	}
 
